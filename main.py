ce4163f775db80ed7404ced78efdfeaec1396164
Manoj <55833119+bmanoj49@users.noreply.github.com>
2025-11-08 17:56:15 +0530

    model trainer implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience_project import logger
 from src.datascience_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience_project.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -33,3 +34,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_training = ModelTrainerTrainingPipeline()
+   model_training.initiate_model_training()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
---
+++ src/datascience_project/pipeline/model_trainer_pipeline.py
@@ -0,0 +1,15 @@
+from src.datascience_project.config.configuration import ConfigurationManager
+from src.datascience_project.components.model_trainer import ModelTrainer
+from src.datascience_project import logger
+
+STAGE_NAME = "Model Trainer Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_training(self):
+        config=ConfigurationManager()
+        model_trainer_config=config.get_model_trainer_config()
+        model_trainer_config=ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
